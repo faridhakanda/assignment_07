b47bb4974ca15da1e7fd8892a0692bd99757f21b
faridhakanda <faridakanda1729@gmail.com>
2026-04-13 22:00:27 +0600

    assignment-07 is successfully setup

---
+++ src/components/header.tsx
@@ -0,0 +1,18 @@
+'use client'
+import { FriendContext } from '@/context/FriendContext';
+import React, { useContext } from 'react';
+
+const Header = () => {
+    const {faridName, names} = useContext(FriendContext);
+    return (
+        <div>
+            <h2>Header file! </h2>
+            <p>{faridName}</p>
+            {names.map((Name) => <div key={Name.id}>
+                <h2>{Name.id}.{Name.name} - {Name.age}</h2>
+            </div>)}
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/context/FriendContext.tsx
@@ -0,0 +1,46 @@
+'use client'
+import { createContext, useState } from "react";
+
+export const FriendContext = createContext();
+
+const FriendProvider = ({children}) => {
+    const names = [
+        {
+            id: 1,
+            name: 'Farid Akanda',
+            age: 29
+        },
+        {
+            id: 2,
+            name: 'Zaheed Sabur',
+            age: 40
+        },
+        {
+            id: 3,
+            name: 'Tamim Shahriar',
+            age: 42
+        },
+        {
+            id: 4,
+            name: 'Zulkernine Mahmud',
+            age: 37
+        },
+        {
+            id: 5,
+            name: 'Mahbubur Rahman',
+            age: 35
+        }
+    ]
+    const [friends, setFriends] = useState([]);
+    const faridName = "Farid Akanda";
+    const friendDetailInfo = {
+        friends, 
+        setFriends,
+        faridName,
+        names
+    }
+    return <FriendContext.Provider value={friendDetailInfo}>
+        {children}
+    </FriendContext.Provider>
+}
+export default FriendProvider;
