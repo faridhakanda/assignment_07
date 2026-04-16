1e7cae2517b2bef880d447f0475159759b2635f1
faridhakanda <faridakanda1729@gmail.com>
2026-04-16 16:37:26 +0600

    now is large device footer is almost completed after fixing bug and issue

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -10,13 +10,14 @@ const Footer = () => {
             <div className='w-280 mx-auto text-center'>
                 <div className='pt-20 '>
                     
-                    <Image className='flex mx-auto py-2' src={KeenKeeper} alt="keenkeeper text logo" width="auto" height="auto" />
+                    <Image className='flex mx-auto py-2' src={KeenKeeper} alt="keenkeeper text logo"  />
+                    
                     <p className='py-1'>Your personal shelf of meaningful connections. Browse, tend, and nurture the relationships that matter most.</p>
                     <h2 className='font-medium text-[20px] py-2'>Social Links</h2>
                     <div className='flex mx-auto justify-center space-x-2'>
-                        <Image src={Instragram} alt="Instragram Icon" width="auto" hegiht="auto" />
-                        <Image src={Facebook} alt="Facebook Icon" width="auto" height="auto" />
-                        <Image src={X} alt="X Icon" width="auto" height="auto" />
+                        <Image src={Instragram} alt="Instragram Icon"  />
+                        <Image src={Facebook} alt="Facebook Icon"  />
+                        <Image src={X} alt="X Icon"  />
                     </div>
                 </div>
                 <hr className='border-t-1 border-gray-600 w-[1120px] mx-auto my-4'></hr>
