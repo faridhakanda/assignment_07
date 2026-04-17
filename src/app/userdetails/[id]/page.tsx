020be29b845c001e99fc820a2233f4f46b2d600b
faridhakanda <faridakanda1729@gmail.com>
2026-04-17 17:21:28 +0600

    friends details page is partially ready

--- src/app/userdetails/[id]/page.tsx
+++ src/app/userdetails/[id]/page.tsx
@@ -43,13 +43,78 @@ const UserDetailsCard = () => {
     }
     return (
         <div>
-            <h2>{currentUser.id}</h2>
+            {/* <h2>{currentUser.id}</h2>
             <h2>{currentUser.name}</h2>
             <Image src={currentUser.picture} alt={currentUser.name} width={200} height={200} />
             <h2>{currentUser.bio}</h2>
             <h2>{currentUser.email}</h2>
             <h2>{currentUser.next_due_date}</h2>
-            <Link href="/">Go to Home</Link>
+            <Link href="/">Go to Home</Link> */}
+            <div className='bg-blue-300  mx-auto justify-around grid grid-cols-1 md:grid-cols-2'>
+                <div className='bg-yellow-100 w-62'>
+                    <div>
+                        <Image 
+                            className='w-[100] h-[100]  rounded-full'
+                            src={currentUser.picture}
+                            alt={currentUser.name}
+                            width={0}
+                            height={0}
+                        />
+                        <h2>{currentUser.name}</h2>
+                        <p>{currentUser.status}</p>
+                        <div>
+                            {currentUser.tags.map((tag, index) =>
+                                <li key={index}>{tag.toUpperCase()}</li> 
+                            )}
+                        </div>
+                        <p>{currentUser.bio}</p>
+                    </div>
+                    
+                    <h2>Snooze 2 Weeks</h2>
+                    <h2>Archive</h2>
+                    <h2>Delete</h2>
+                </div>
+                <div className='bg-lime-300'>
+                    <div className='flex text-center space-x-2'>
+                        <div>
+                            <h2>{currentUser.days_since_contact}</h2>
+                            <p className='text-[12px]'>Days Since Contact</p>
+                        </div>
+                        <div>
+                            <h2>{currentUser.goal}</h2>
+                            <p className='text-[12px]'>Goal(Days)</p>
+                        </div>
+                        <div>
+                            <h2>{currentUser.next_due_date}</h2>
+                            <p className='text-[12px]'>Next Due</p>
+                        </div>
+                    </div>
+                    <div className='flex bg-yellow-200 justify-between mx-4 '>
+                        <div>
+                            <h2>Relationship Goal</h2>
+                            <p>Connect every <span>30 days</span></p>
+                        </div>
+                        <button>Edit</button>
+                    </div>
+                    <div>
+                        <h2>Quick Check-In</h2>
+                        <div className='flex space-x-2 text-center items-center mx-auto'>
+                            <div>
+                                <Image src="/call.png" alt="call icon" width={32} height={32} />
+                                <button>Call</button>
+                            </div>
+                            <div>
+                                <Image src="/text.png" alt="call icon" width={32} height={32} />
+                                <button>Text</button>
+                            </div>
+                            <div>
+                                <Image src="/video.png" alt="call icon" width={32} height={32} />
+                                <button>Video</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/userCard.tsx
+++ src/components/userCard.tsx
@@ -33,7 +33,7 @@ const UserCard = ({user}: UserDataType) => {
                     height={0}
                 />
                 <h2 className='text-[16px] my-2'>{user.name}</h2>
-                <p className='text-[#647489] text-[14px]'>63d ago</p>    
+                <p className='text-[#647489] text-[14px]'>{user.days_since_contact}dago</p>    
                 <div className='flex'>
                     {user.tags.map((tag, index) => 
                         <li className='list-none text-[12px] mx-auto my-2 bg-green-300 px-2 py-1 rounded-full w-fit' key={index}>{tag.toUpperCase()}</li>
